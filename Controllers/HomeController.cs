f07db56b6336e23e5853a17a17717520b647d64c
Umut KAHRAMAN <kahraman.umut@hotmail.com>
2018-01-22 01:55:17 +0300

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstagramStalker.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            /*GetProfile profile = new GetProfile("hopehero3");
+            ViewBag.profilePicture = profile.ZoomPictureLink();*/
+            return View();
+        }
+
+        [HttpPost]
+        public string GetPhoto(string userName)
+        {
+            GetProfile profile = new GetProfile(userName);
+            return profile.ZoomPictureLink();
+        }
+
+    }
+}
---
+++ GetProfile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace InstagramStalker
+{
+    public class GetProfile
+    {
+        string instagramLink = "https://www.instagram.com/";
+        string userName = "";
+
+        public GetProfile(string UserName)
+        {
+            userName = UserName;
+        }
+
+        private string GetHtmlContent()
+        {
+            Uri url = new Uri(instagramLink + userName);
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            string html = client.DownloadString(url);
+            return html;
+        }
+
+        private string GetPictureLink()
+        {
+            string htmlDoc = GetHtmlContent();
+            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+            document.LoadHtml(htmlDoc);
+            var node = document.DocumentNode.SelectNodes("//meta");
+            return node[14].Attributes[1].Value;
+        }
+
+        public string ZoomPictureLink()
+        {
+            string pictureLink = GetPictureLink();
+            return pictureLink.Replace("/s150x150", ""); 
+        }
+
+    }
+}
